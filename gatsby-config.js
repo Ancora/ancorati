d8f55206dc2347ec54c285ddf8414cf5311d5f50
Ancora <anderson.rocha@ancora-ti.com.br>
2019-08-28 23:00:23 -0300

    Organização inicial.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Âncora Soluções em TI`,
+    description: `Homepage oficial da empresa Âncora Soluções em TI, especializada em sites para pequenas empresas e profissionais liberais.`,
+    author: `@ancorati`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
